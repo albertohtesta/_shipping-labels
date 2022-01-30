30d7b5b2a98aaca8135abdefe091075d5c2fb635
albert hdez <albertohtesta@gmail.com>
2022-01-29 20:39:17 -0600

    App routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "main#index"
+
+  resources :solicitudes, only: [:index, :create, :show] do
+    resources :shippings, only: [:index] do
+    end
+  end
+
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :shippings, only: [:index]
+      resources :solicitudes, only: [:create, :index]
+      get '/status', to: 'solicitudes#status'
+      get '/download_pdf', to: "solicitudes#download_pdf"
+    end
+  end
+  
 end
